28fae475878b2afc3cb4729034333663dabf5fb2
monolok <antoinebertin@me.com>
2024-03-28 18:11:55 +0100

    done

--- fast_api.py
+++ fast_api.py
@@ -1,1 +1,9 @@
 # branch api
+
+# curl -X 'POST' \
+#   'https://get-around.onrender.com/predict' \
+#   -H 'accept: application/json' \
+#   -H 'Content-Type: application/json' \
+#   -d '{
+# "data": [["Renault", 186382, 120, "diesel", "silver", "estate", 1, 1, 0, 0, 0, 0, 1]]
+# }'
