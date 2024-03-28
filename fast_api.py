587146801122fab344e88e666939065790dd3693
monolok <antoinebertin@me.com>
2024-03-28 17:14:05 +0100

    forgot

--- fast_api.py
+++ fast_api.py
@@ -12,12 +12,12 @@ class InputData(BaseModel):
 def predict(data: InputData):
     preprocessor = load('/Users/antoinebertin/Documents/jedha/full_stack/projects_full_stack/deploy_ml/preprocessor.joblib')
     model = load('/Users/antoinebertin/Documents/jedha/full_stack/projects_full_stack/deploy_ml/model.joblib')
-    transformed_data = preprocessor.transform(pd.DataFrame(data.data))
+    #user_test = ['Renault', 186382, 120, 'diesel', 'silver', 'estate', 1, 1, 0, 0,0, 0, 1]
+    user_test = data.data[0]
+    print('######################MSG###########')
+    print(user_test)
+    features = ['model_key', 'mileage', 'engine_power', 'fuel', 'paint_color', 'car_type', 'private_parking_available', 'has_gps', 'has_air_conditioning', 'automatic_car', 'has_getaround_connect', 'has_speed_regulator', 'winter_tires']
+    data = pd.DataFrame([user_test], columns=features)
+    transformed_data = preprocessor.transform(data)
     prediction = model.predict(transformed_data)
     return {'prediction': prediction.tolist()}
-
-# Index(['model_key', 'mileage', 'engine_power', 'fuel', 'paint_color',
-#        'car_type', 'private_parking_available', 'has_gps',
-#        'has_air_conditioning', 'automatic_car', 'has_getaround_connect',
-#        'has_speed_regulator', 'winter_tires'],
-#       dtype='object')
